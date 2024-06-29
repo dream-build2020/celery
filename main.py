27f3a207cf6d410f89f171538c837ab1500d1650
dream-build2020 <kindtom123@gmail.com>
2024-06-30 00:05:33 +0800

    新增celery项目： 1、增加定时任务

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from config import loggin_config
-from tasks.tasks import Tasks
+from tasks import tasks
 
 LOGGER = loggin_config.LOGGER
 
 if __name__ == "__main__":
-    Tasks.task1.apply_async(kwargs={'a': 1, 'b': 2})
+    tasks.task1.apply_async(kwargs={'a': 1, 'b': 2})
     pass
