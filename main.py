8f449fd18237e4e80710ff1fed0c2c43a9c2af86
dream-build2020 <kindtom123@gmail.com>
2024-06-29 16:20:09 +0800

    开启celery项目

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from tasks.tasks import Tasks
 LOGGER = loggin_config.LOGGER
 
 if __name__ == "__main__":
-    Tasks.task1.apply_async(kwarg={'a': 1, 'b': 2})
+    Tasks.task1.apply_async(kwargs={'a': 1, 'b': 2})
     pass
