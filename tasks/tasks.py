7f3b9d6036b5e2aecfb52a4f639a6e74806e8cb2
dream-build2020 <kindtom123@gmail.com>
2024-06-29 15:25:30 +0800

    开启celery项目

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -4,6 +4,6 @@ from config import celery_app
 class Tasks:
     @staticmethod
     @celery_app.tasks
-    def task1(a: int, b: int) -> int:
+    def task1(a: int, b: int):
         restart = a + b
         return restart
