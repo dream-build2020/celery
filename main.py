b922b73c76d4cd837279ec60583465218b795995
dream-build2020 <kindtom123@gmail.com>
2024-06-29 14:49:55 +0800

    开启celery项目

---
+++ main.py
@@ -0,0 +1,8 @@
+from config import loggin_config
+from tasks.tasks import Tasks
+
+LOGGER = loggin_config.LOGGER
+
+if __name__ == "__main__":
+    Tasks.task1.apply_async(kwarg={'a': 1, 'b': 2})
+    pass
---
+++ tasks/tasks.py
@@ -0,0 +1,9 @@
+from config.celery_config import celery_app
+
+
+class Tasks:
+    @staticmethod
+    @celery_app.tasks
+    def task1(a: int, b: int) -> int:
+        restart = a + b
+        return restart
