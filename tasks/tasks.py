1e6c7b3cfe08cbc35eb9de8b83a21ea0cfed5ff6
dream-build2020 <kindtom123@gmail.com>
2024-06-29 23:16:16 +0800

    新增celery项目： 1、增加定时任务

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,9 +1,7 @@
 from config import celery_app
 
 
-class Tasks:
-    @staticmethod
-    @celery_app.task
-    def task1(a, b):
-        restart = a + b
-        return restart
+@celery_app.task
+def task1(a, b):
+    restart = a + b
+    return restart
