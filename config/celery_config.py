d7d170eba5e3046f89ac7abcfb2a167f2fc8f859
dream-build2020 <kindtom123@gmail.com>
2024-06-29 15:39:05 +0800

    开启celery项目

--- config/celery_config.py
+++ config/celery_config.py
@@ -1,5 +1,5 @@
-BROKER_URL = 'redis://localhost:6379/0'  # 指定 Broker
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'  # 指定 Backend
+BROKER_URL = 'redis://192.168.31.101/0'  # 指定 Broker
+CELERY_RESULT_BACKEND = 'redis://192.168.31.101/1'  # 指定 Backend
 
 CELERY_TIMEZONE = 'Asia/Shanghai'  # 指定时区，默认是 UTC
 # CELERY_TIMEZONE='UTC'
